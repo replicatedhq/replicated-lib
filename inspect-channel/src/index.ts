d26af87023a531568f26668a765a5462f729c827
Josh De Winne <josh@dewinne.dev>
2023-05-09 14:15:55 -0700

    Using npm module alpha12

--- inspect-channel/src/index.ts
+++ inspect-channel/src/index.ts
@@ -1,10 +1,18 @@
 import * as core from '@actions/core';
 import { getChannelDetails } from 'replicated-lib';
+import { VendorPortalApi } from 'replicated-lib/dist/configuration';
 
 
 async function run() {
   try {
-    const channel = await getChannelDetails(core.getInput('replicated-app'), core.getInput('channel-name'))
+    const apiToken = core.getInput('replicated-api-token')
+    const appSlug = core.getInput('replicated-app')
+    const channelName = core.getInput('channel-name')
+    
+    const apiClient = new VendorPortalApi();
+    apiClient.apiToken = apiToken;
+
+    const channel = await getChannelDetails(apiClient, appSlug, channelName)
 
     core.setOutput('channel-id', channel.id);
     core.setOutput('channel-slug', channel.slug);
