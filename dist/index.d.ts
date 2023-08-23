76c24b4b9f4a623a6953abe5e1dd4f512d65ef5c
Josh De Winne <josh@dewinne.dev>
2023-08-23 10:00:43 -0700

    Getting ready for releasing beta.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,4 @@ export { getApplicationDetails } from './applications';
 export { Channel, createChannel, getChannelDetails, archiveChannel } from './channels';
 export { ClusterVersion, createCluster, pollForStatus, getKubeconfig, removeCluster, getClusterVersions } from './clusters';
 export { KubernetesDistribution, archiveCustomer, createCustomer, getUsedKubernetesDistributions } from './customers';
-export { Release, createRelease, createReleaseFromChart, promoteRelease, reportCompatibilityResult } from './releases';
+export { Release, CompatibilityResult, createRelease, createReleaseFromChart, promoteRelease, reportCompatibilityResult } from './releases';
