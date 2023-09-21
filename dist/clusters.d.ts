dadd94f2f4398498ad6337c87c47c1a4128ad8dc
Craig O'Donnell <craig@replicated.com>
2023-09-21 09:31:30 -0400

    add disk, node, and instance type for create cluster

--- dist/clusters.d.ts
+++ dist/clusters.d.ts
@@ -8,7 +8,7 @@ export declare class ClusterVersion {
     name: string;
     version: string;
 }
-export declare function createCluster(vendorPortalApi: VendorPortalApi, clusterName: string, k8sDistribution: string, k8sVersion: string, clusterTTL: string): Promise<Cluster>;
+export declare function createCluster(vendorPortalApi: VendorPortalApi, clusterName: string, k8sDistribution: string, k8sVersion: string, clusterTTL: string, diskGib?: number, nodeCount?: number, instanceType?: string): Promise<Cluster>;
 export declare function pollForStatus(vendorPortalApi: VendorPortalApi, clusterId: string, expectedStatus: string, timeout?: number): Promise<Cluster>;
 export declare function getKubeconfig(vendorPortalApi: VendorPortalApi, clusterId: string): Promise<string>;
 export declare function removeCluster(vendorPortalApi: VendorPortalApi, clusterId: string): Promise<void>;
