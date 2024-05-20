3a37154a04503cb3c90fb51ebc22dab1ded53e1b
Josh De Winne <joris.dewinne@gmail.com>
2024-05-20 12:43:36 -0700

    remove dist

--- dist/configuration.d.ts
+++
@@ -1,6 +0,0 @@
-import * as httpClient from "@actions/http-client";
-export declare class VendorPortalApi {
-    endpoint: string;
-    apiToken: string;
-    client(): Promise<httpClient.HttpClient>;
-}
