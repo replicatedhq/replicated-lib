4bf236a14d9edbb6cbbcd401f15a29dcebc69444
Xav Paice <xav@replicated.com>
2025-11-27 16:04:44 +1300

    add customer list function

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { VendorPortalApi } from "./configuration";
 export { getApplicationDetails } from "./applications";
 export { Channel, createChannel, getChannelDetails, archiveChannel, pollForAirgapReleaseStatus, getDownloadUrlAirgapBuildRelease } from "./channels";
 export { ClusterVersion, createCluster, createClusterWithLicense, pollForStatus, getKubeconfig, removeCluster, upgradeCluster, getClusterVersions, createAddonObjectStore, pollForAddonStatus, exposeClusterPort } from "./clusters";
-export { KubernetesDistribution, archiveCustomer, createCustomer, getUsedKubernetesDistributions } from "./customers";
+export { KubernetesDistribution, CustomerSummary, archiveCustomer, createCustomer, getUsedKubernetesDistributions, listCustomersByName } from "./customers";
 export { Release, CompatibilityResult, createRelease, createReleaseFromChart, promoteRelease, reportCompatibilityResult } from "./releases";
