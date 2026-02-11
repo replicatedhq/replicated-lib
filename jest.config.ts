8dc931f4052f8791c6676cf28dee836fbe24ef40
Gerard Nguyen <gerard@replicated.com>
2026-02-11 13:38:53 +1100

    fix test

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,31 @@
 const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     testMatch: ['**/test/**/*.+(ts|tsx)', '**/src/**/(*.)+(spec|test).+(ts|tsx)'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'esbuild-jest',
-    },
     collectCoverageFrom: [
       '**/src/*.{ts,tsx}',
       '!**/node_modules/**',
       '!**/vendor/**',
       '!**/dist/**'
     ],
+    moduleDirectories: ['node_modules', 'src'],
+    transformIgnorePatterns: [
+      'node_modules/(?!@actions)'
+    ],
+    moduleNameMapper: {
+      '^@actions/http-client$': '<rootDir>/node_modules/@actions/http-client/lib/index.js'
+    },
+    transform: {
+      '^.+\\.tsx?$': ['ts-jest', {
+        useESM: false,
+        tsconfig: {
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true
+        }
+      }],
+      '^.+\\.jsx?$': ['ts-jest', {
+        useESM: false
+      }]
+    }
 }
 export default config
